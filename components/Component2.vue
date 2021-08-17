7df28862f92a6649b666c8a31f3956f6d0c8f178
Firenzo <firenzo_akkefietje@msn.com>
2021-08-17 17:40:06 +0200

    added sass functionality

---
+++ components/Component2.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Component 2</h1>
+</template>
+
+<style scoped lang="scss">
+    h1{
+        color:tomato;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <Tutorial />
+<div class="content">
+  <Component1 />
+  <Component2 />
+</div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss">
+  @import '~/assets/styles/main.scss';
+</style>
