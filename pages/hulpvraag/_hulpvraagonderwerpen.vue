d25c81c8e01a405497a99fd417b31ae14062414e
Jorden <firenzo.jorden@capgemini.com>
2021-08-31 14:58:29 +0200

    removed <Fragment> plugin due to issues with page transitions

--- pages/hulpvraag/_hulpvraagonderwerpen.vue
+++ pages/hulpvraag/_hulpvraagonderwerpen.vue
@@ -1,12 +1,7 @@
 <template>
-  <Fragment>
     <div class="container">
       <h1>dynamic page</h1>
     </div>
-    <div class="container 2">
-      <h1>hoi</h1>
-    </div>
-  </Fragment>
 </template>
 
 <style scoped lang="scss">
