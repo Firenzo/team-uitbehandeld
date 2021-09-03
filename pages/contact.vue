02052d59b85c3b5198174eba0251c9ac2048dd90
Jorden <firenzo.jorden@capgemini.com>
2021-09-03 16:56:23 +0200

    finalized contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,12 +2,13 @@
   <main>
     <div class="container">
       <h1>Contact</h1>
-      <div class="map-location">
-
-      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolorum itaque cumque commodi asperiores exercitationem magni quis nisi voluptas? Necessitatibus!</p>
+      <div class="map-location"></div>
 
       <form action="">
+        <label for="email">Email:</label>
         <input type="email" id="email" name="email" placeholder="Email" />
+        <label for="message">Jouw bericht:</label>
         <textarea id="subject" name="subject" placeholder="Type je bericht..."></textarea>
         <input type="submit" value="Verstuur" />
       </form>
@@ -36,23 +37,39 @@ main{
   div.container{
     max-width:550px;
 
+    @include min-900{
+      max-width:1200px;
+      display:grid;
+      column-gap:40px;
+      row-gap:40px;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 35px fit-content(2px) 1fr;
+    }
+
     div.map-location{
       background:gray;
-      height:200px;
+      height:300px;
       width:100%;
       margin-bottom:20px;
+
+      @include min-900{
+        grid-row-start: 2;
+      }
     }
 
     form{
       display:flex;
       flex-wrap:wrap;
+      flex-direction:column;
+      grid-column-start:2;
+      grid-row-start:2;
+
+      label{
+        margin-bottom:10px;
+      }
 
       input[type="email"],textarea{
         margin-bottom:15px;
-      }
-
-      >*{
-        flex-basis:100%;
       }
     }
   }
