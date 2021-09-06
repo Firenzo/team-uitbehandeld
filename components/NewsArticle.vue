56eeac2b122ce3d637f575b1895569aee53c7e61
Ken <kienyanneko@gmail.com>
2021-09-06 10:24:49 +0200

    add grid

--- components/NewsArticle.vue
+++ components/NewsArticle.vue
@@ -1,11 +1,47 @@
 <template>
-  <li>test hallo</li>
+  <li>
+    <div class="grid-element">
+      <p class="date-and-author">Date | Name author</p>
+      <h1>Titel</h1>
+      <p class="content">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem a in,
+        delectus quae voluptate accusamus enim optio cupiditate excepturi autem
+        similique incidunt ad architecto.
+      </p>
+    </div>
+  </li>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss">
-@use "styles/main";
+<style scoped lang="scss">
+@use "styles/main" as *;
+
+li {
+  list-style: none;
+  padding: 20px 10px;
+  border: solid 1px $light-green;
+  div.grid-element {
+    p.date-and-author {
+      word-spacing: 2px;
+      margin-bottom: 15px;
+    }
+  }
+  p.content {
+    height: 120px;
+    overflow: hidden;
+    color: $light-text-color;
+  }
+  p.date-and-author {
+    color: $light-text-color;
+  }
+  h1 {
+    margin-bottom: 15px;
+    color: $light-text-color;
+    font-weight: 400;
+    font-size: 24px;
+  }
+}
 </style>
