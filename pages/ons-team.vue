62a9e7d2eb689164b1f46d27f7832ec69865bdac
Ken <kienyanneko@gmail.com>
2021-09-09 12:01:34 +0200

    finish ons team page

--- pages/ons-team.vue
+++ pages/ons-team.vue
@@ -6,7 +6,7 @@
         <li v-for="teamMember in teamMembers" :key="teamMember.id" class="member">
           <div class="name-and-role">
             <div class="image">
-              <img :src="`http://localhost:1338${teamMember.photo.url}`" />
+              <img :src="teamMember.photo ? `http://localhost:1338${teamMember.photo.url}` : ''" />
             </div>
           </div>
           <div class="title-and-role">
@@ -15,105 +15,6 @@
             <p class="second-role">{{ teamMember.short_description }}</p>
             <p class="role-description">
               {{ teamMember.long_description }}
-            </p>
-          </div>
-        </li>
-        <li class="member">
-          <div class="name-and-role">
-            <div class="image">
-              <img src="~/assets/images/Team_WoutMiddelman.png" />
-            </div>
-          </div>
-          <div class="title-and-role">
-            <h2>Wout Middelman</h2>
-            <p class="role">Bestuurslid</p>
-            <p class="second-role">
-              Eigenaar Middelman wonen, partner van zijn uitbehandelde vrouw en
-              vader van 4 kinderen en oprichter van de stichting
-              teamuitbehandeld.nl
-            </p>
-            <p class="role-description">
-              “Mij helpt het verwerken door me in te zetten, dan krijgt het
-              verdriet een plek.” Echtgenote Mirjam (54) van de Soester
-              ondernemer Wout Middelman en lid van Rotary Soest-Baarn, heeft in
-              april 2019 te horen gekregen dat ze medisch is uitbehandeld. ,,Dat
-              is een harde boodschap. Het ziekenhuis laat je vervolgens los en
-              verwijst je naar je huisarts die niet altijd alle antwoorden
-              heeft. En als je googled op ‘uitbehandeld’ vind je geen
-              informatie.” Reden voor Wout Middelman Teamuitbehandeld.nl op te
-              richten”
-            </p>
-          </div>
-        </li>
-        <li class="member">
-          <div class="name-and-role">
-            <div class="image">
-              <img src="~/assets/images/Team_MettySpelt.png" />
-            </div>
-          </div>
-          <div class="title-and-role">
-            <h2>Metty Spelt</h2>
-            <p class="role">Voorzitter</p>
-            <p class="second-role">Programmanager Gezondheid Wonen Inc.</p>
-            <p class="role-description">
-              “Als je gezond bent kun je je moeilijk voorstellen hoe het is om
-              ernstig ziek te zijn en helemaal moeilijk voor te stellen als je
-              dan de mededeling te krijgen dat je uitbehandeld bent. En dan is
-              het ineens heel dichtbij als de wereld van dierbare vrienden
-              opeens op z’n kopt staat. Het verdriet, de ontgoocheling de
-              toekomst die er niet meer samen is en alle vragen die geen
-              antwoorden hebben. Het platform van Teamuitbehandeld gaat
-              antwoorden geven op die vragen die zo belangrijk zijn in de laste
-              fase van het leven. Daar ga ik mij voor inzetten”!
-            </p>
-          </div>
-        </li>
-        <li class="member">
-          <div class="name-and-role">
-            <div class="image">
-              <img src="~/assets/images/Team_PieterMul.png" />
-            </div>
-          </div>
-          <div class="title-and-role">
-            <h2>Pieter Mul</h2>
-            <p class="role">Penningmeester</p>
-            <p class="second-role">
-              Accountant en algemeen directeur en partner van Jan© Accountants
-              en adviseurs B.V.
-            </p>
-            <p class="role-description">
-              “Om hulp vragen betekent niet dat je opgeeft, zei het paard. Het
-              betekent dat je weigert op te geven.” Een prachtige quote uit “De
-              Jongen, de Mol, de Vos en het Paard” van Charlie Mackesy. Met ons
-              platform hoop ik dat we veel antwoorden kunnen geven op hulpvragen
-              van uitbehandelde patiënten. Met mijn financiële achtergrond draag
-              ik graag mijn steentje bij aan de Stichting Teamuitbehandeld.nl.
-            </p>
-          </div>
-        </li>
-        <li class="member">
-          <div class="name-and-role">
-            <div class="image">
-              <img src="~/assets/images/Team_JacquelineDrop-Pullens.png" />
-            </div>
-          </div>
-          <div class="title-and-role">
-            <h2>Jacqueline Drop-Pullens</h2>
-            <p class="role">Secretaris</p>
-            <p class="second-role">
-              Rouw- en verlies- begeleider, eigenaar van stoerkracht
-            </p>
-            <p class="role-description">
-              “Alles waar je ogen van gaan sprankelen moet je doen!” Wanneer je
-              het bericht krijgt dat je uitbehandeld bent eindigt er iets voor
-              de boodschapper en begint er een periode voor de persoon (en
-              omgeving) die dit te horen krijgt. Uit ervaring als rouw en
-              verliestherapeut weet ik hoe belangrijk het is dat er op dat
-              moment ook een soort houvast gerecreëerd kan worden. Iets waar jij
-              gezien en gehoord wordt met jouw vraag. Team Uitbehandeld heeft
-              een platform ontwikkeld die jou en je naasten hierin kunnen
-              ondersteunen. Mijn energie en expertise inzetten dit te creëren,
-              is waar mijn ogen en hart van gaan sparkelen!!
             </p>
           </div>
         </li>
