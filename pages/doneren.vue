e85a2dd218246a722dda461a0243df9607cade99
Jorden <firenzo.jorden@capgemini.com>
2021-09-28 17:12:02 +0200

    donation price input validation

--- pages/doneren.vue
+++ pages/doneren.vue
@@ -7,7 +7,7 @@
           <label for="">Kies zelf een donatiebedrag:</label>
           <div class="inputfield-for-price">
             <Fa-icon :icon="['fas', 'euro-sign']" />
-            <input type="text" v-model="donationAmount" placeholder="Donatiebedrag" @keypress="checkNumber($event)" @input="adaptString()">
+            <input type="text" v-model="donationAmountString" placeholder="Donatiebedrag" @keypress="checkNumber($event)" @input="adaptString()">
             <p v-if="donationAmount">{{donationAmount}}</p>
           </div>
         </div>
@@ -19,7 +19,8 @@
 <script>
 export default {
   data: () => ({
-    donationAmount: null
+    donationAmount: null,
+    donationAmountString: ''
   }),
 
   methods: {
@@ -29,15 +30,45 @@ export default {
         console.log(parseInt(event.key))
       }
 
-      if (event.key === ',' && /[,-]/.test(this.donationAmount)) {
+      if (event.key === ',' && /[,-]/.test(this.donationAmountString)) {
         event.preventDefault()
+      }
+
+      console.log(this.donationAmountString.indexOf(','))
+      console.log(this.donationAmountString.substring(0, this.donationAmountString.indexOf(',')).length)
+
+      if (this.donationAmountString.includes(',')) {
+        // code for when the string has a comma
+        if (this.donationAmountString.substring(0, this.donationAmountString.indexOf(',')).length > 17 && event.key !== ',' && event.target.selectionEnd < this.donationAmountString.indexOf(',')) {
+          console.log(event.target.selectionEnd)
+          console.log(this.donationAmountString.indexOf(','))
+          console.log('case 1')
+          event.preventDefault()
+        }
+
+        if (this.donationAmountString.substring(this.donationAmountString.indexOf(','), this.donationAmountString.length - 1).length >= 2 && event.target.selectionEnd > this.donationAmountString.indexOf(',')) {
+          console.log('case 2')
+          console.log(this.donationAmountString.substring(this.donationAmountString.indexOf(','), this.donationAmountString.length))
+          event.preventDefault()
+        }
+      }
+
+      if (!this.donationAmountString.includes(',')) {
+        // code for when the string has no comma
+        if (this.donationAmountString.length > 17 && event.key !== ',') {
+          event.preventDefault()
+        }
       }
     },
 
     adaptString () {
-      if (this.donationAmount === ',') {
-        this.donationAmount = '0,'
+      if (this.donationAmountString === ',') {
+        this.donationAmountString = '0,'
       }
+
+      this.donationAmount = this.donationAmountString.replace(/\./g, '')
+      this.donationAmountString = this.donationAmount.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+      console.log('without seperators:', this.donationAmount)
     }
   }
 }
