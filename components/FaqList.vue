80c375faff9f7b6e838f9b6a1714a65a162e88f1
maartendek <maarten.dekker@protonmail.com>
2021-10-13 11:50:03 +0200

    faqs

--- components/FaqList.vue
+++ components/FaqList.vue
@@ -1,21 +1,38 @@
 <template>
   <section class="questions-and-answers">
     <div class="container">
-      <h2>{{subject}}</h2>
-      <div v-for="subjectQuestion in subjectQuestions" :key="subjectQuestion.id" class="faq">
+      <h2 v-if="subject">{{subject}}</h2>
+      <div v-for="subjectQuestion in subjectQuestionsDisplayed" :key="subjectQuestion.id" class="faq">
         <h3>{{subjectQuestion.question}}?</h3>
         <div v-html="$md.render(subjectQuestion.answer)" class="md-container"></div>
       </div>
+      <NuxtLink v-if="showMoreLink" to="/hulpvraag/faq" class="standalone-link">Meer veelgestelde vragen<Fa-icon :icon="['fas', 'arrow-right']" /></NuxtLink>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  props: ['subjectQuestions', 'subject'],
+  props: {
+    max: {
+      type: Number,
+      default: 99
+    },
+    page: {
+      type: String,
+      default: 'faq'
+    },
+    subjectQuestions: Array,
+    subject: String
+  },
 
   computed: {
-
+    subjectQuestionsDisplayed () {
+      return this.subjectQuestions.slice(0, this.max)
+    },
+    showMoreLink () {
+      return (this.subjectQuestions.length > this.max && this.page !== 'faq')
+    }
   },
   mounted () {
 
@@ -27,6 +44,8 @@ export default {
 @use "styles/main" as *;
 section.questions-and-answers{
   margin-top:50px;
+  margin-bottom:50px;
+
   div.container{
     h2{
       margin-bottom:20px;
@@ -41,6 +60,16 @@ section.questions-and-answers{
       h3{
         font-size:18px;
         font-weight:400;
+      }
+    }
+
+    a.standalone-link{
+      display:block;
+      text-align:center;
+      color:gray;
+
+      svg{
+        margin-left:7px;
       }
     }
   }
