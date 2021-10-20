100b916b2ed45439df6b82eb36fd64495a637005
Jorden <firenzo.jorden@capgemini.com>
2021-10-20 16:29:48 +0200

    escaped parentheses

--- copy-nuxt-project.sh
+++ copy-nuxt-project.sh
@@ -1,4 +1,4 @@
 # run "git add --chmod=+x copy-nuxt-project.sh" to make the file executeable when update
-cd '../team-uitbehandeld-strapi/public/' && rm -rv !('uploads') && cp -Rv '../../team-uitbehandeld/dist/.' .
+cd '../team-uitbehandeld-strapi/public/' && rm -rv !\('uploads'\) && cp -Rv '../../team-uitbehandeld/dist/.' .
 cd '../../team-uitbehandeld/'
 exit
