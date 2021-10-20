9683c094f58a1e0dd7d33b3fbb1c540caf8f2044
Jorden <firenzo.jorden@capgemini.com>
2021-10-20 17:12:26 +0200

    find command

--- copy-nuxt-project.sh
+++ copy-nuxt-project.sh
@@ -1,5 +1,4 @@
 # run "git add --chmod=+x copy-nuxt-project.sh" to make the file executeable when update
-# cd '../team-uitbehandeld-strapi/public/' && find * -maxdepth 0 -name 'uploads' -prune -o -exec rm -rv '{}' ';' && cp -Rv '../../team-uitbehandeld/dist/.' .
-cd '../team-uitbehandeld-strapi/public/' && find * -maxdepth 0 -name 'uploads' -prune -o -exec rm -rv '{}' ';'
+cd '../team-uitbehandeld-strapi/public/' && find * -maxdepth 0 -name 'uploads' -prune -o -exec rm -rv '{}' ';' && cp -Rv '../../team-uitbehandeld/dist/.' .
 cd '../../team-uitbehandeld/'
 exit
