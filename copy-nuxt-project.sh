c3e0288a64aab2bb01cb623c76a2075bad709ffb
Jorden <firenzo.jorden@capgemini.com>
2021-10-20 12:31:11 +0200

    added command to delete old files

--- copy-nuxt-project.sh
+++ copy-nuxt-project.sh
@@ -1,3 +1,4 @@
 # run "git add --chmod=+x copy-nuxt-project.sh" to make the file executeable when update
+rm -r ../team-uitbehandeld-strapi/public/*
 cp -R dist/. '../team-uitbehandeld-strapi/public/'
 exit
