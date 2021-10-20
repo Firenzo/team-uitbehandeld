d5a5838108808258898c6fa078680cb8f514cc91
Jorden <firenzo.jorden@capgemini.com>
2021-10-20 17:10:22 +0200

    test if files get deleted

--- copy-nuxt-project.sh
+++ copy-nuxt-project.sh
@@ -1,4 +1,5 @@
 # run "git add --chmod=+x copy-nuxt-project.sh" to make the file executeable when update
-cd '../team-uitbehandeld-strapi/public/' && find * -maxdepth 0 -name 'uploads' -prune -o -exec rm -rv '{}' ';' && cp -Rv '../../team-uitbehandeld/dist/.' .
+# cd '../team-uitbehandeld-strapi/public/' && find * -maxdepth 0 -name 'uploads' -prune -o -exec rm -rv '{}' ';' && cp -Rv '../../team-uitbehandeld/dist/.' .
+cd '../team-uitbehandeld-strapi/public/' && find * -maxdepth 0 -name 'uploads' -prune -o -exec rm -rv '{}' ';'
 cd '../../team-uitbehandeld/'
 exit
