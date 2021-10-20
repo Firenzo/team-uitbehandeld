336d3234973750cb0a4bdc6e7f88c6da05dfed59
Jorden <firenzo.jorden@capgemini.com>
2021-10-20 14:06:30 +0200

    optimized copy command

--- copy-nuxt-project.sh
+++ copy-nuxt-project.sh
@@ -1,3 +1,4 @@
 # run "git add --chmod=+x copy-nuxt-project.sh" to make the file executeable when update
-cd '../team-uitbehandeld-strapi/public/' && rm -rv !("uploads") && cp -R '../../team-uitbehandeld/dist/.' .
+cd '../team-uitbehandeld-strapi/public/' && rm -rv !('uploads') && cp -Rv '../../team-uitbehandeld/dist/.' .
+cd '../../team-uitbehandeld/'
 exit
